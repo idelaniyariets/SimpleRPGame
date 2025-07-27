cc243e34e07dfa1fd500e92cec7ad56d10bd89c4
idealniyariets <mr.kriper724@yandex.ru>
2025-07-27 16:35:26 +0300

    mvp developed

--- game_re/Scenes/Level_5/level_5.gd
+++ game_re/Scenes/Level_5/level_5.gd
@@ -10,9 +10,15 @@ func _ready() -> void:
 	GlobalVar.can_move = false
 	get_tree().create_tween().tween_property($light/DirectionalLight2D, "energy", 0.5, 5)
 	await get_tree().create_timer(5).timeout
+	$CanvasLayer/AnimatedSprite2D2.visible = true
 	label.text = "и что это было... ох"
 	await get_tree().create_timer(1).timeout
-	label.text = ""
+	get_tree().create_tween().parallel().tween_property(label, "text", "", 0.5)
+	get_tree().create_tween().parallel().tween_property(label, "modulate", Color8(255, 255, 255, 0), 0.5)
+	label.modulate = Color8(255,255,255,255)
+	get_tree().create_tween().parallel().tween_property($CanvasLayer/AnimatedSprite2D2, "modulate", Color8(255,255,255,0), 0.5)
+	$CanvasLayer/AnimatedSprite2D2.modulate = Color8(255,255,255,255)
+	$CanvasLayer/AnimatedSprite2D2.visible = false
 	GlobalVar.can_move = true
 	GlobalVar.level = 5
 	if GlobalVar.loaded == true:
